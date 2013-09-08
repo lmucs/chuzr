1bfcff9c978b17beac0ea89c6a2843f57ca4f98a
Ray Toal <rtoal@lmu.edu>
2013-09-08 16:51:11 -0700

    Clean up web client stubs

--- web-client/app.js
+++ web-client/app.js
@@ -1,10 +1,9 @@
 /* chuzr web client */
 
 var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var stylus = require('stylus')
 
 var app = express();
 
@@ -15,16 +14,17 @@ app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
+app.use(express.cookieParser('zombie feynman'));
+app.use(express.session());
 app.use(app.router);
-app.use(require('stylus').middleware(__dirname + '/public'));
+app.use(stylus.middleware(path.join(__dirname, '/public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-if ('development' == app.get('env')) {
+if ('development' === app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+require('./controllers/main')(app);
 
 http.createServer(app).listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
