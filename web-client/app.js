30a877ebd08e6dd43b2bf56c358bbc31edd2b826
Ray Toal <rtoal@lmu.edu>
2013-09-10 22:39:08 -0700

    Use favicon

--- web-client/app.js
+++ web-client/app.js
@@ -8,16 +8,16 @@ var stylus = require('stylus')
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.favicon());
+app.use(express.favicon(path.join(__dirname, 'public/icons/favicon.png')));
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(express.cookieParser('zombie feynman'));
 app.use(express.session());
 app.use(app.router);
-app.use(stylus.middleware(path.join(__dirname, '/public')));
+app.use(stylus.middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 if ('development' === app.get('env')) {
