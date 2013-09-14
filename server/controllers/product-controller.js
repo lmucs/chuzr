dc5c2cfa9ac95853910576386b0bdc8c90b830de
frodosamoa <andrewrkowalczyk@gmail.com>
2013-09-14 15:49:08 -0700

    made the firts initial version of the product controller

--- server/controllers/product-controller.js
+++ server/controllers/product-controller.js
@@ -1,4 +1,43 @@
+User = require('../models/product')
+
 module.exports = function (app) {
 
-  // TODO
+  // TODO: Needs to be an HTTP 400 eventually.  Actually consider middleware validator.
+  function validateId(id) {
+    if (/\D/.test(id)) {
+      throw Error('Illegal id')
+    }
+    return id
+  }
+
+  app.get('/products', function (req, res) {
+    res.send('Finding products');
+  })
+
+  app.post('/products', function (req, res) {
+    res.send('Creating a product');
+  })
+
+  app.get('/products/:id', function (req, res) {
+    id = validateId(req.params.id);
+    try {
+      res.json(Product.findById(id));
+    } catch (e) {
+      if (e == Product.NO_SUCH_PRODUCT) {
+        res.send(400, 'No such product');
+      } else {
+        throw e;
+      }     
+    }
+  })
+
+  app.put('/products/:id', function (req, res) {
+    id = validateId(req.params.id)
+    res.send('Updating product ' + id);
+  })
+
+  app.delete('/productd/:id', function (req, res) {
+    id = validateId(req.params.id)
+    res.send('Deleting product ' + id);
+  })
 }
