3d9cd1a68b758077d652742b86ef3678285cfca8
Ray Toal <rtoal@lmu.edu>
2013-09-18 00:44:30 -0700

    Trying to diagnose weird behavior on this branch; master is ok

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,8 +1,5 @@
 User = require('../models/user')
 
-console.log("HI FROM THE USER CONTROLLER");
-console.log(typeof(User.findById))
-
 module.exports = function (app) {
 
   // TODO: Needs to be an HTTP 400 eventually.  Actually consider middleware validator.
@@ -14,10 +11,6 @@ module.exports = function (app) {
   }
 
   app.get('/users', function (req, res) {
-    console.log(Object.keys(User))
-    console.log(User.findAll)
-    console.log(User.findAll.length)
-    console.log(User.findAll(0, 5))
     res.json(User.findAll(0, 100))
   })
 
@@ -25,18 +18,10 @@ module.exports = function (app) {
   })
 
   app.get('/users/:id', function (req, res) {
-    console.log('Getting user')
     id = +validateId(req.params.id);
     try {
-      console.log('Okay so far')
-      console.log(typeof(id))
-      o = User.findAll(0, 3)
-      console.log("found")
-      console.log(JSON.stringify(o));
-      console.log("---")
-      res.json(o);
+      res.json(User.findById(id));
     } catch (e) {
-      console.log(e == User.NO_SUCH_USER)
       if (e == User.NO_SUCH_USER) {
         res.send(400, 'No such user');
       } else {
@@ -55,40 +40,3 @@ module.exports = function (app) {
     res.send('Deleting user ' + id);
   })
 }
-
-new User({
-  loginName: "alice", 
-  displayName: "Alice Cooper",
-  passwordHash: "3948rh3498fh3498h",
-  profile: {interests: ["music", "out of school"], sex: "M"},
-  reputation: 100
-});
-
-new User({
-  loginName: "bob", 
-  displayName: "Bob Martin",
-  passwordHash: "3948rh4njrfh3498h",
-  profile: {interests: ["clean code", "sushi chef"], sex: "M"},
-  reputation: 10000000
-});
-
-new User({
-  loginName: "cassandra", 
-  displayName: "Cassandra Cooper",
-  passwordHash: "1111rh4njrfh3498h",
-  profile: {interests: [], sex: "F"},
-  reputation: 1099999
-});
-
-u = new User({
-  loginName: "dusty", 
-  displayName: "Dusty Street",
-  passwordHash: "11441q4njrfh3498h",
-  profile: {interests: ["music", "cars"], sex: "F"},
-  reputation: 109
-});
-
-console.log(User.findAll(0, 4))
-
-for (p in u) {console.log(p)}
-
--- server/models/user.js
+++ server/models/user.js
@@ -1,9 +1,6 @@
 // This currently uses a mock users array just to get things up and running.
 // Students will need to replace this with a real MongoDB.
 
-var mockUsers = [];
-var maxId = 0;
-
 var VALID_PROPERTIES = [
   'loginName', 'displayName', 'passwordHash', 'profile', 
   'reputation'
@@ -14,16 +11,15 @@ function validateUserData(userData) {
   // Students will implement
 }
 
-function User(userData) {
-  validateUserData(userData);
-  VALID_PROPERTIES.forEach(function (property) {
-    if (property in userData) {
-      this[property] = userData[property];
-    }
-  }, this);
+module.exports = User = function (userData) {
+  // validateUserData(userData);
+  // VALID_PROPERTIES.forEach(function (property) {
+  //   if (property in userData) {
+  //     this[property] = userData[property];
+  //   }
+  // }, this);
   this.id = maxId++;
   mockUsers.push(this);
-  console.log(mockUsers)
 };
 
 Object.defineProperty(User, 'NO_SUCH_USER', {
@@ -33,20 +29,13 @@ Object.defineProperty(User, 'NO_SUCH_USER', {
   value: {}
 });
 
-console.log('sdgsdgsdgsdgsdgsdgsdgsdgsd')
-User.gh = function (x) {return 1};
-
 User.findAll = function (skip, limit) {
     // TODO - limit clamp
-    //return mockUsers.slice(skip, skip + limit);
-    console.log("dddddd")
-    console.log(mockUsers.length)
-    return [4, 5, 6, 9]
+    return mockUsers.slice(skip, skip + limit);
 };
 
 User.findById = function (id) {
     // HAHA OBVIOUSLY THIS IS NOT THE REAL FINAL CODE
-    console.log("WOOOOOOOOOO")
     for (var i = 0; i < mockUsers.length; i++) {
         if (+id === mockUsers[i].id) {
             console.log("found %s id", id)
@@ -65,8 +54,38 @@ User.prototype.delete = function (id) {
     
 };
 
-console.log("HI FROM THE USER MODULE");
+// TODO Stuff that belongs in test.
+var mockUsers = [];
+var maxId = 0;
 
-module.exports = User;
+new User({
+  loginName: "alice", 
+  displayName: "Alice Cooper",
+  passwordHash: "3948rh3498fh3498h",
+  profile: {interests: ["music", "out of school"], sex: "M"},
+  reputation: 100
+});
 
+new User({
+  loginName: "bob", 
+  displayName: "Bob Martin",
+  passwordHash: "3948rh4njrfh3498h",
+  profile: {interests: ["clean code", "sushi chef"], sex: "M"},
+  reputation: 10000000
+});
 
+new User({
+  loginName: "cassandra", 
+  displayName: "Cassandra Cooper",
+  passwordHash: "1111rh4njrfh3498h",
+  profile: {interests: [], sex: "F"},
+  reputation: 1099999
+});
+
+new User({
+  loginName: "dusty", 
+  displayName: "Dusty Street",
+  passwordHash: "11441q4njrfh3498h",
+  profile: {interests: ["music", "cars"], sex: "F"},
+  reputation: 109
+});
