0bdbc2870a2280fec25091971d686a748cedee7f
Ray Toal <rtoal@lmu.edu>
2013-09-17 21:41:59 -0700

    Some hacks on users

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,5 +1,8 @@
 User = require('../models/user')
 
+console.log("HI FROM THE USER CONTROLLER");
+console.log(typeof(User.findById))
+
 module.exports = function (app) {
 
   // TODO: Needs to be an HTTP 400 eventually.  Actually consider middleware validator.
@@ -11,18 +14,29 @@ module.exports = function (app) {
   }
 
   app.get('/users', function (req, res) {
-    res.send('Finding users');
+    console.log(Object.keys(User))
+    console.log(User.findAll)
+    console.log(User.findAll.length)
+    console.log(User.findAll(0, 5))
+    res.json(User.findAll(0, 100))
   })
 
   app.post('/users', function (req, res) {
-    res.send('Creating a user');
   })
 
   app.get('/users/:id', function (req, res) {
-    id = validateId(req.params.id);
+    console.log('Getting user')
+    id = +validateId(req.params.id);
     try {
-      res.json(User.findById(id));
+      console.log('Okay so far')
+      console.log(typeof(id))
+      o = User.findAll(0, 3)
+      console.log("found")
+      console.log(JSON.stringify(o));
+      console.log("---")
+      res.json(o);
     } catch (e) {
+      console.log(e == User.NO_SUCH_USER)
       if (e == User.NO_SUCH_USER) {
         res.send(400, 'No such user');
       } else {
@@ -42,7 +56,39 @@ module.exports = function (app) {
   })
 }
 
-new User("alice");
-new User("bob");
-new User("cassandra");
-new User("dinh");
+new User({
+  loginName: "alice", 
+  displayName: "Alice Cooper",
+  passwordHash: "3948rh3498fh3498h",
+  profile: {interests: ["music", "out of school"], sex: "M"},
+  reputation: 100
+});
+
+new User({
+  loginName: "bob", 
+  displayName: "Bob Martin",
+  passwordHash: "3948rh4njrfh3498h",
+  profile: {interests: ["clean code", "sushi chef"], sex: "M"},
+  reputation: 10000000
+});
+
+new User({
+  loginName: "cassandra", 
+  displayName: "Cassandra Cooper",
+  passwordHash: "1111rh4njrfh3498h",
+  profile: {interests: [], sex: "F"},
+  reputation: 1099999
+});
+
+u = new User({
+  loginName: "dusty", 
+  displayName: "Dusty Street",
+  passwordHash: "11441q4njrfh3498h",
+  profile: {interests: ["music", "cars"], sex: "F"},
+  reputation: 109
+});
+
+console.log(User.findAll(0, 4))
+
+for (p in u) {console.log(p)}
+
--- server/models/user.js
+++ server/models/user.js
@@ -4,37 +4,69 @@
 var mockUsers = [];
 var maxId = 0;
 
-module.exports = User = function (name) {
-    this.name = name;
-    this.id = maxId++;
-    mockUsers.push(this);
+var VALID_PROPERTIES = [
+  'loginName', 'displayName', 'passwordHash', 'profile', 
+  'reputation'
+];
+
+function validateUserData(userData) {
+  // throw exception if something is bad
+  // Students will implement
 }
+
+function User(userData) {
+  validateUserData(userData);
+  VALID_PROPERTIES.forEach(function (property) {
+    if (property in userData) {
+      this[property] = userData[property];
+    }
+  }, this);
+  this.id = maxId++;
+  mockUsers.push(this);
+  console.log(mockUsers)
+};
 
 Object.defineProperty(User, 'NO_SUCH_USER', {
-    configurable: false,
-    enumerable: false,
-    writable: false,
-    value: {}
+  configurable: false,
+  enumerable: false,
+  writable: false,
+  value: {}
 });
 
+console.log('sdgsdgsdgsdgsdgsdgsdgsdgsd')
+User.gh = function (x) {return 1};
+
 User.findAll = function (skip, limit) {
-    return mockUsers.slice(skip, skip + limit);
-}
+    // TODO - limit clamp
+    //return mockUsers.slice(skip, skip + limit);
+    console.log("dddddd")
+    console.log(mockUsers.length)
+    return [4, 5, 6, 9]
+};
 
 User.findById = function (id) {
     // HAHA OBVIOUSLY THIS IS NOT THE REAL FINAL CODE
+    console.log("WOOOOOOOOOO")
     for (var i = 0; i < mockUsers.length; i++) {
         if (+id === mockUsers[i].id) {
+            console.log("found %s id", id)
             return mockUsers[i];
         }
     }
+    console.log("darn")
     throw User.NO_SUCH_USER;
-}
+};
 
 User.prototype.save = function (id, userData) {
 
-}
+};
 
 User.prototype.delete = function (id) {
     
-}
+};
+
+console.log("HI FROM THE USER MODULE");
+
+module.exports = User;
+
+
