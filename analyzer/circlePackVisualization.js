f2ce49cf541d8ec2da0840250a421bfacfa01dde
Joe Crawley <joe.crawley10@gmail.com>
2013-09-22 21:23:27 -0700

    reverting commit

--- analyzer/circlePackVisualization.js
+++ analyzer/circlePackVisualization.js
@@ -17,22 +17,6 @@ var vis = d3.select("body").insert("svg:svg", "h2")
     .attr("height", h)
     .append("svg:g")
     .attr("transform", "translate(" + (w - r) / 2 + "," + (h - r) / 2 + ")");
-    
-var color = function(node) {
-	var rating = node.rating,
-	    b = 0,
-        r = 255*2*(1-rating),
-        g = 255*2*rating;
-        
-    if (r > 255) {
-        r = 255;
-    }
-    if (g > 255) {
-        g = 255;
-    }
-
-	return "rgb(" + Math.floor(r) + "," + Math.floor(g) + "," + Math.floor(b) + ")";
-}
 
 //d3.json("circlePackTestData.json", function(data) {
 createTestData(function (data) {    
@@ -42,51 +26,26 @@ createTestData(function (data) {
     vis.selectAll("circle")
         .data(nodes)
         .enter().append("svg:circle")
-        .attr("class", function(d) { 
-            return d.children ? "parent" : "child"; 
-        })
-        .attr("cx", function(d) { 
-            return d.x; 
-        })
-        .attr("cy", function(d) { 
-            return d.y;
-        })
-        .attr("r", function(d) { 
-            return d.r;
-        })
-        .style("fill", function(d) { 
-            return color(d);
-        })        
-        //.style("fill", "rgb(255,0,0)")
-        .on("click", function(d) { 
-            return zoom(node == d ? root : d);
-        });
+        .attr("class", function(d) { return d.children ? "parent" : "child"; })
+        .attr("cx", function(d) { return d.x; })
+        .attr("cy", function(d) { return d.y; })
+        .attr("r", function(d) { return d.r; })
+        .on("click", function(d) { return zoom(node == d ? root : d); });
 
     vis.selectAll("text")
         .data(nodes)
         .enter().append("svg:text")
-        .attr("class", function(d) { 
-            return d.children ? "parent" : "child";
-        })
-        .attr("x", function(d) { 
-            return d.x;
-        })
-        .attr("y", function(d) { 
-            return d.y;
-        })
+        .attr("class", function(d) { return d.children ? "parent" : "child"; })
+        .attr("x", function(d) { return d.x; })
+        .attr("y", function(d) { return d.y; })
         .attr("dy", ".35em")
         .attr("text-anchor", "middle")
-        .style("opacity", function(d) { 
-            return d.r > 20 ? 1 : 0;
-        })
-        .text(function(d) { 
-            return d.name;
-        });
+        .style("opacity", function(d) { return d.r > 20 ? 1 : 0; })
+        .text(function(d) { return d.name; });
 
     d3.select(window).on("click", function() {
-        zoom(root);
+        zoom(root); });
     });
-});
 
 function zoom(d, i) {
     var k = r / d.r / 2;
@@ -97,65 +56,50 @@ function zoom(d, i) {
         .duration(d3.event.altKey ? 7500 : 750);
 
     t.selectAll("circle")
-        .attr("cx", function(d) { 
-            return x(d.x);
-        })
-        .attr("cy", function(d) { 
-            return y(d.y);
-        })
-        .attr("r", function(d) { 
-            return k * d.r;
-        });
+        .attr("cx", function(d) { return x(d.x); })
+        .attr("cy", function(d) { return y(d.y); })
+        .attr("r", function(d) { return k * d.r; });
 
     t.selectAll("text")
-        .attr("x", function(d) { 
-            return x(d.x);
-        })
-        .attr("y", function(d) { 
-            return y(d.y);
-        })
-        .style("opacity", function(d) { 
-            return k * d.r > 20 ? 1 : 0;
-        });
+        .attr("x", function(d) { return x(d.x); })
+        .attr("y", function(d) { return y(d.y); })
+        .style("opacity", function(d) { return k * d.r > 20 ? 1 : 0; });
 
     node = d;
     d3.event.stopPropagation();
 }
 
 function createTestData(callback) {
-    var data = {
-            "name": "products",
-            "children": [],
-            "size": 10000,
-            "level": 0,
-            "rating": Math.random()
-        }, 
-        addRandomChildren = function (parent) {
-            var childrenNum = Math.floor(Math.random() * 10 / parent.level),
-                children = [],
-                child,
-                i;
-            
-            for (i = 0; i < childrenNum; i++) {
-                child = {
-                    "name": "child_" + i,
-                    "level": parent.level + 1,
-                    "rating": Math.random()
-                };
-                child.size = Math.floor(Math.random() * 10000 / Math.pow(2, child.level)) 
-                             + 0.5 * 10000 / Math.pow(2, child.level);
-                child.children = addRandomChildren(child);
-                children.push(child);
-            }
-            
-            return children;
-        };
-    
+	var data = {
+        "name": "products",
+        "children": [],
+        "size": 10000,
+        "level": 0
+    }, 
+    addRandomChildren = function (parent) {
+        var childrenNum = Math.floor(Math.random() * 10 / parent.level),
+            children = [],
+            child,
+            i;
+        
+        for (i = 0; i < childrenNum; i++) {
+            child = {
+                "name": "child_" + i,
+                "level": parent.level + 1
+            };
+            child.size = Math.floor(Math.random() * 10000 / Math.pow(2, child.level)) 
+                         + 0.5 * 10000 / Math.pow(2, child.level);
+            child.children = addRandomChildren(child);
+            children.push(child);
+        }
+        
+        return children;
+    };
+
     ["Sports", "Aparel", "Electronics"].forEach(function (category) {
         var child = {
             "name": category,
-            "level": 1,
-            "rating": Math.random()
+            "level": 1
         };
         
         child.size = Math.floor(Math.random() * 10000 / Math.pow(2, child.level)) 
