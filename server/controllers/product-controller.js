9de6e8468bb5b71bb8ea0d94d6c6d155eba6693c
Ray Toal <rtoal@lmu.edu>
2013-09-26 09:30:58 -0700

    Make product controller use product models

--- server/controllers/product-controller.js
+++ server/controllers/product-controller.js
@@ -1,3 +1,6 @@
+Product = require('../models/product')
+
+
 //Created using http://pixelhandler.com/blog/2012/02/09/develop-a-restful-api-using-node-js-with-express-and-mongoose/
 // as reference.
 module.exports = function (app) {
@@ -15,7 +18,7 @@ module.exports = function (app) {
         skip = +req.query.skip || 0; 
         limit = +req.query.limit || 10; 
         console.log('skip = %d, limit = %d', skip, limit);
-        res.json(User.findAll(skip=skip, limit=limit));
+        res.json(Product.findAll(skip=skip, limit=limit));
     });
     
     //Create a new project
