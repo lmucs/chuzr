cfd5c90099cb7e0e657b9ad88ad0ee821c05e43d
Jasmine Dahilig <jasmine.dahilig@gmail.com>
2013-09-26 11:15:15 -0700

    added mongoose connection to server

--- server/app.js
+++ server/app.js
@@ -1,8 +1,9 @@
-/* chuzr web service */
-
 var express = require('express');
 var http = require('http');
-var mongoose = require('mongoose')
+var mongoose = require('mongoose');
+var fs = require('fs');
+var env = process.env.NODE_ENV || 'development';
+var config = require('./config/config')[env];
 
 var app = express();
 
@@ -10,18 +11,28 @@ app.set('port', process.env.PORT || 3000);
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
-//app.use(express.cookieParser('zombie marie curie'));
-//app.use(express.session());
 
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-// TODO - configure mongoose
+mongoose.connect(config.db);
 
+// Bootstrap models
+// fs.readdirSync(__dirname + '/models').forEach(function (file) {
+//   if (~file.indexOf('.js')) {
+//     require(__dirname + '/models/' + file);
+//   }
+// });
+require('./models/user');
+
+// Bootstrap controllers
+// fs.readdirSync(__dirname + '/controllers').forEach(function (file) {
+//   if (~file.indexOf('.js')) {
+//     require(__dirname + '/controllers/' + file)(app);
+//   }
+// });
 require('./controllers/user-controller')(app)
-require('./controllers/product-controller')(app)
-require('./controllers/vote-controller')(app)
 
 http.createServer(app).listen(app.get('port'), function () {
   console.log('Express server listening on port ' + app.get('port'));
