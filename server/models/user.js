acd9724c1250eb076611379822548a206678b198
Jasmine Dahilig <jasmine.dahilig@gmail.com>
2013-09-29 23:14:17 -0700

    cleaned up user model

--- server/models/user.js
+++ server/models/user.js
@@ -2,39 +2,21 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var userSchema = new Schema({
-  name: { type: String, default: '' },
-  email: { type: String, default: ''},
-  hashed_password: { type: String, default: '' }
+  name: {
+    first: String,
+    last: { type: String, trim: true }
+  },
+  email: String,
+  username: String,
+  hashed_password: String,
+  salt: String
 });
 
 var User = mongoose.model('User', userSchema);
 
-// Clear out old data
+// Clear old data
 User.remove({}, function(err) {
   if (err) {
     console.log ('error deleting old data.');
   }
 });
-
-// Creating one user.
-var johndoe = new User ({
-  name: { first: 'John', last: '  Doe   ' },
-  age: 25
-});
-
-// Saving it to the database.
-johndoe.save(function (err) {if (err) console.log ('Error on save!')});
-
-// Creating more users manually
-var janedoe = new User ({
-  name: { first: 'Jane', last: 'Doe' },
-  age: 65
-});
-janedoe.save(function (err) {if (err) console.log ('Error on save!')});
-
-// Creating more users manually
-var alicesmith = new User ({
-  name: { first: 'Alice', last: 'Smith' },
-  age: 45
-});
-alicesmith.save(function (err) {if (err) console.log ('Error on save!')});
