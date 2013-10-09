5f598677ab3612104a9bb052eed88273813a9c55
Thomas Costello <tomfcostello@gmail.com>
2013-10-08 21:33:27 -0700

    Updated with changes added in mongoose branch

--- server/controllers/coupon-controller.js
+++ server/controllers/coupon-controller.js
@@ -9,73 +9,42 @@ module.exports = function (app) {
     return id;
   };
 
-  /** Gets all coupons **/
   app.get('/coupons', function (req, res) {
-    var parameterFlag = false,
-        coupons = [];
-    if (req.query.issuer) {
-      coupons.push.apply(coupons, Coupon.findByIssuer(req.query.issuer));
-      parameterFlag = true;
-    }
-	
-    //"coupons/?status=active" will show "valid" (not expired) coupons, "status=expired" will show expired coupons
-	if (req.query.status) {
-      coupons.push.apply(coupons, Coupon.findByStatus(req.query.status));
-      parameterFlag = true;
-    }
-	
-    if (!parameterFlag) {
-      console.log("Status Code: ", res.statusCode);
-      skip = +req.query.skip || 0;
-      limit = +req.query.limit || 10;
-      console.log('skip = %d, limit = %d', skip, limit);
-      return res.json(Coupon.findAll(skip=skip, limit=limit));
-    }
-    return res.json(coupons);
-  });
-
-  app.get('/coupons/:id', function (req, res) {
-    var id = validateCouponId(req.params.id);
-    try {
-        res.json(Coupon.findById(id));
-    } catch (e) {
-        if (e == Coupon.NO_SUCH_COUPON) {
-            res.send(400, 'No such coupon');
-        } else {
-            throw e;
-        }     
-    }
+    Coupon.find(function (err, coupons) {
+      if (!err) {
+        return res.send(coupons);
+      } else {
+        return console.log(err);
+      }
+    });
   });
   
-  /** Creates a coupon **/
-  app.post('/coupons', function (req, res) {
-	var coupon = new Coupon(req.body)//{
-	res.send(201, coupon);
-  });
-
-  /** Updates a coupon **/
-  app.put('/coupons/:id', function (req, res) {
-    id = validateCouponId(req.params.id);
-	var newData = req.body;
-	try {
-	  coupon = Coupon.findById(id);
-	} catch(e) {
-	  res.send(400, "can't update a nonexisting coupon");
-	}
-	Coupon.update(coupon, newData);
-	res.send(200);
-  });
-
-  app.delete('/coupons/:id', function (req, res) {
-    id = validateCouponId(req.params.id);
-    coupon = Coupon.findById(id);
-    Coupon.delete(id);
-    res.send("Coupon Deleted");
-    /*
-    coupon.remove(function (error) {
-      return error ? res.send(error) : res.send("coupon removed");
+  app.get('/coupons/:id', function (req, res) {
+    return Coupon.findById(req.params.id, function (err, coupons) {
+      if (!err) {
+        return res.send(coupons);
+    }
+      else {
+        return res.send(404, 'No Such Coupon');
+      }
     });
-    */
+  });
+  
+  app.post('/coupons', function (req, res) {
+    var coupon = new Coupon({
+      issuer: req.body.issuer,
+      value: req.body.value,
+      promoCode: req.body.promoCode,
+      expirationDate: req.body.expirationDate,
+      imageURL: req.body.imageURL
+    });
+    coupon.save(function (err) {
+      if (!err) {
+        return console.log('Coupon Added');
+      } else {
+        return console.log(err);
+      }
+    });
+    return res.send(coupon);
   });
 }
-  
