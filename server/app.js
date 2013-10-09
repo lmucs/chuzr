4dcc7b7d87ac377a10c148d7e9954f15d836949b
Ray Toal <rtoal@lmu.edu>
2013-10-09 09:13:27 -0700

    Load models and controllers synchronously

--- server/app.js
+++ server/app.js
@@ -2,44 +2,38 @@ var express = require('express'),
     http = require('http'),
     fs = require('fs'),
     mongoose = require('mongoose'),
-    cors = require('cors'),
-    env = process.env.NODE_ENV || 'development',
-    config = require('./config/config')[env];
+    cors = require('cors');
 
-var app = express();
-
+console.log('Configuring the Chuzr API');
+var env = process.env.NODE_ENV || 'development',
+    config = require('./config/config')[env],
+    app = express();
 app.set('port', process.env.PORT || 3000);
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(cors());
-
-if ('development' == app.get('env')) {
+if ('development' === app.get('env')) {
   app.use(express.errorHandler());
 }
 
+console.log('Connecting to Mongo at %s', config.db)
 mongoose.connect(config.db);
 
-// Load models
-fs.readdir(__dirname + '/models', function (err, files) {
-  if (err) console.log(err);
-  files.forEach(function (file) {
-    if (file.match('.js$')) {
-      require(__dirname + '/models/' + file);
-    }
-  });
+console.log('Loading models')
+fs.readdirSync(__dirname + '/models').forEach(function (file) {
+  if (file.match('.js$')) {
+    require(__dirname + '/models/' + file);
+  }
 });
 
-// Load controllers
-fs.readdir(__dirname + '/controllers', function (err, files) {
-  if (err) console.log(err);
-  files.forEach(function (file) {
-    if (file.match('.js$')) {
-      require(__dirname + '/controllers/' + file)(app);
-    }
-  });
+console.log('Loading controllers')
+fs.readdirSync(__dirname + '/controllers').forEach(function (file) {
+  if (file.match('.js$')) {
+    require(__dirname + '/controllers/' + file)(app);
+  }
 });
 
 http.createServer(app).listen(app.get('port'), function () {
-  console.log('Express server listening on port ' + app.get('port'));
+  console.log('Chuzr API running on port %s, environment=%s', app.get('port'), env);
 });
