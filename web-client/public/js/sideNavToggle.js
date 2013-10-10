e361f1217e4de62f914fb00d255e82866a57e4d8
Kaitlyn Higa <khiga1@lion.lmu.edu>
2013-10-10 10:22:43 -0700

    added hover functionality to side nav toggle

--- web-client/public/js/sideNavToggle.js
+++ web-client/public/js/sideNavToggle.js
@@ -1,18 +1,32 @@
-var sideNavShown = true;
-          sideNavToggle = function(){
-            $("#side-nav-toggle").click(function(){
-              if(sideNavShown){              
-                $("#side-nav").hide(100);
-                $("#toggle-caret").text("▶");
-                $("#main-content").css("width", "95%");
-                sideNavShown = false;
-              } else {
-                $("#side-nav").show(100);
-                $("#toggle-caret").text("◀");
-                $("#main-content").css("width", "70%");
-                sideNavShown = true;
-              }
-            });
+$(function(){
+  var sideNavShown = true;
+  $("#side-nav-toggle").hover(
+      function(){
+        $("#side-nav-toggle").css("width", "20px");
+        if(sideNavShown){
+          $("#toggle-caret").text("◀");
+        }else{
+          $("#toggle-caret").text("▶");          
+        }
+      },
+      function(){
+        $("#side-nav-toggle").css("width", "5px");
+        $("#toggle-caret").text("");
+      });
 
-          };
-      sideNavToggle();
+  $("#side-nav-toggle").click(function(){
+    if(sideNavShown){              
+      $("#side-nav").hide(100);
+      $("#main-content").css("width", "95%");
+      sideNavShown = false;
+    } else {
+      $("#side-nav").show(100);
+      $("#main-content").css("width", "70%");
+      sideNavShown = true;
+    }
+  });
+
+
+
+
+});
