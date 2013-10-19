b36b2defd1c0d93c5f9bc6ced06610833625acca
Haley Young <haleyyoung88@gmail.com>
2013-10-19 15:36:01 -0700

    Added skip and limit tests to vote

--- server/test/vote.js
+++ server/test/vote.js
@@ -36,36 +36,48 @@ var voteFive = {
   productId: 2,
   rating: 3
 };
+
 var voteSix = {
   userId: 1,
   productId: 44,
   rating: 8
 };
+
 var voteSeven = {
   userId: 12,
   productId: 9,
   rating: 10
 };
+
 var voteEight = {
   userId: 8,
   productId: 8,
   rating: 8
 };
+
 var voteNine = {
   userId: 123,
   productId: 456,
   rating: 7
 };
+
 var voteTen = {
   userId: 288,
   productId: 46,
   rating: 6
 };
+
 var voteEleven = {
   userId: 65,
   productId: 789,
   rating: 1
 };
+
+var voteTwelve = {
+  userId: 99,
+  productId: 98,
+  rating: 0
+}
 
 describe('Votes Model', function(){
 
@@ -115,7 +127,7 @@ describe('Votes Controller', function(){
       })
     })
     it('should return a list of three votes', function (done) {
-      // Create the votes.
+      // Create 3 votes.
       request(url).post('/votes').send(voteOne).end(function (err, res) {
         if (err) throw err;
         res.should.have.status(201);
@@ -150,7 +162,7 @@ describe('Votes Controller', function(){
     })
     
     it('should return two votes with productId = 32', function (done) {
-      // Create the votes.
+      // Create 4 votes.
       request(url).post('/votes').send(voteOne).end(function (err, res) {
         if (err) throw err;
         res.should.have.status(201);
@@ -187,7 +199,7 @@ describe('Votes Controller', function(){
     })
     
     it('should return two votes with userId = 0', function (done) {
-      // Create the votes.
+      // Create 4 votes.
       request(url).post('/votes').send(voteOne).end(function (err, res) {
         if (err) throw err;
         res.should.have.status(201);
@@ -224,7 +236,7 @@ describe('Votes Controller', function(){
     })
     
     it('should return one vote with userId = 0 and productId = 32', function (done) {
-      // Create the votes.
+      // Create 4 votes.
       request(url).post('/votes').send(voteOne).end(function (err, res) {
         if (err) throw err;
         res.should.have.status(201);
@@ -319,6 +331,169 @@ describe('Votes Controller', function(){
         res.should.have.status(200)
         console.log(res.body)
         res.body.length.should.equal(10);
+        done();
+      })
+    })
+    
+    it('should return a list of 3 votes, testing limit', function (done) {
+      // Create 4 votes
+      request(url).post('/votes').send(voteOne).end(function (err, res) {
+        if (err) throw err;
+        res.should.have.status(201);
+      })
+
+      request(url).post('/votes').send(voteTwo).end(function (err, res) {
+        if (err) throw err;
+        res.should.have.status(201);
+      })
+
+      request(url).post('/votes').send(voteThree).end(function (err, res) {
+        if (err) throw err;
+        res.should.have.status(201);
+      })
+
+      request(url).post('/votes').send(voteFour).end(function (err, res) {
+        if (err) throw err;
+        res.should.have.status(201);
+      })
+
+      // Get the first 3 votes.
+      request(url).get('/votes?limit=3').end(function (err, res) {
+        if (err) throw err;
+        res.should.have.status(200);
+        res.body[0].userId.should.eql(1);
+        res.body[0].productId.should.eql(32);
+        res.body[0].rating.should.eql(8);
+        res.body[1].userId.should.eql(0);
+        res.body[1].productId.should.eql(32);
+        res.body[1].rating.should.eql(4);
+        res.body[2].userId.should.eql(0);
+        res.body[2].productId.should.eql(15);
+        res.body[2].rating.should.eql(9);
+        res.body.length.should.equal(3);
+        done();
+      })
+    })
+
+    it('should return a list of 10 votes starting with the 2nd vote in the db, testing skip and limit',
+      function (done) {
+      // Create 12 votes
+      request(url).post('/votes').send(voteOne).end(function (err, res) {
+        if (err) throw err;
+        res.should.have.status(201);
+      })
+
+      request(url).post('/votes').send(voteTwo).end(function (err, res) {
+        if (err) throw err;
+        res.should.have.status(201);
+      })
+
+      request(url).post('/votes').send(voteThree).end(function (err, res) {
+        if (err) throw err;
+        res.should.have.status(201);
+      })
+
+      request(url).post('/votes').send(voteFour).end(function (err, res) {
+        if (err) throw err;
+        res.should.have.status(201);
+      })
+
+      request(url).post('/votes').send(voteFive).end(function (err, res) {
+        if (err) throw err;
+        res.should.have.status(201);
+      })
+
+      request(url).post('/votes').send(voteSix).end(function (err, res) {
+        if (err) throw err;
+        res.should.have.status(201);
+      })
+
+      request(url).post('/votes').send(voteSeven).end(function (err, res) {
+        if (err) throw err;
+        res.should.have.status(201);
+      })
+
+      request(url).post('/votes').send(voteEight).end(function (err, res) {
+        if (err) throw err;
+        res.should.have.status(201);
+      })
+
+      request(url).post('/votes').send(voteNine).end(function (err, res) {
+        if (err) throw err;
+        res.should.have.status(201);
+      })
+
+      request(url).post('/votes').send(voteTen).end(function (err, res) {
+        if (err) throw err;
+        res.should.have.status(201);
+      })
+
+      request(url).post('/votes').send(voteEleven).end(function (err, res) {
+        if (err) throw err;
+        res.should.have.status(201);
+      })
+
+      request(url).post('/votes').send(voteTwelve).end(function (err, res) {
+        if (err) throw err;
+        res.should.have.status(201);
+      })
+      
+      // Get 10 votes starting with the second one in the db.
+      request(url).get('/votes?skip=1').end(function (err, res) {
+        if (err) throw err;
+        res.should.have.status(200);
+        res.body[0].userId.should.eql(0);
+        res.body[0].productId.should.eql(32);
+        res.body[0].rating.should.eql(4);
+        res.body[9].userId.should.eql(65);
+        res.body[9].productId.should.eql(789);
+        res.body[9].rating.should.eql(1);
+        res.body.length.should.equal(10);
+        done();
+      })
+    })
+    
+    it('should return a list of 3 votes, testing skip and limit', function (done) {
+      // Create 5 votes
+      request(url).post('/votes').send(voteOne).end(function (err, res) {
+        if (err) throw err;
+        res.should.have.status(201);
+      })
+
+      request(url).post('/votes').send(voteTwo).end(function (err, res) {
+        if (err) throw err;
+        res.should.have.status(201);
+      })
+
+      request(url).post('/votes').send(voteThree).end(function (err, res) {
+        if (err) throw err;
+        res.should.have.status(201);
+      })
+
+      request(url).post('/votes').send(voteFour).end(function (err, res) {
+        if (err) throw err;
+        res.should.have.status(201);
+      })
+
+      request(url).post('/votes').send(voteFive).end(function (err, res) {
+        if (err) throw err;
+        res.should.have.status(201);
+      })
+      
+      // Get 3 votes starting with the second one in the db.
+      request(url).get('/votes?skip=1&limit=3').end(function (err, res) {
+        if (err) throw err;
+        res.should.have.status(200);
+        res.body[0].userId.should.eql(0);
+        res.body[0].productId.should.eql(32);
+        res.body[0].rating.should.eql(4);
+        res.body[1].userId.should.eql(0);
+        res.body[1].productId.should.eql(15);
+        res.body[1].rating.should.eql(9);
+        res.body[2].userId.should.eql(2);
+        res.body[2].productId.should.eql(4);
+        res.body[2].rating.should.eql(10);
+        res.body.length.should.equal(3);
         done();
       })
     })
