572fc33f8e2500f3efaee01965b9b8fb73475b90
Danny Gibson <dgibson8@lion.lmu.edu>
2013-10-22 10:26:57 -0700

    prepping for mongodb loading

--- feed-processor/app.js
+++ feed-processor/app.js
@@ -1,7 +1,11 @@
 console.log('Configuring the Chuzr API');
 var env = process.env.NODE_ENV || 'development',
     config = require('./config/config')[env],
-    ingest = require('./ingestor');
+    ingest = require('./ingestor'),
+    mongoose = require('mongoose');
+
+console.log('Connecting to Mongo at %s', config.db);
+mongoose.connect(config.db);
 
 var url = 'http://www.beso.com/users/MrsClassic/favorites.json';
 ingest.json(url);
