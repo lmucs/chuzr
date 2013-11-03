ecf156fb63f88a73a390ed5f42bfea8555d06d91
Britainjs <bsouthwi@lion.lmu.edu>
2013-11-03 03:13:42 -0800

    Added a hook to hash the password on save using bcrypt and a method to check a password. Implementation is open to change, since there seem to be a variety of ways to do this.

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose')
   , Schema = mongoose.Schema
-  , ObjectId = Schema.ObjectId;
+  , ObjectId = Schema.ObjectId
+  , bcrypt = require('bcrypt')
+  , workFactor = 10;
 
 var userSchema = new Schema({
     name            : {
@@ -19,4 +21,24 @@ var userSchema = new Schema({
   , salt            : String
 });
 
+// A pre hook to hash the password on save.
+userSchema.pre('save', function (next) {
+  var user = this;
+  
+  //don't bother hashing if no changes were made
+  if (!user.isModified('hashedPassword')) return next();
+  
+  bcrypt.hash(pass, workFactor, function(err, hash) {
+    if (err) return next(err);
+    user.hashedPassword = hash;
+    next();
+  });
+};
+
+userSchema.methods.checkPassword = function (entry, callback) {
+  bcrypt.compare(entry, this.password, function (err, isMatch) {
+    if (err) return callback(err);
+    callback(null, isMatch);
+  });
+}
 module.exports = mongoose.model('User', userSchema);
