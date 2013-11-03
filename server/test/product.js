b8f0a33bf966fa6edc6bd29b07f8d1ee733304f9
frodosamoa <andrewrkowalczyk@gmail.com>
2013-11-02 19:21:24 -0700

    updated both product examples to match the new schema, semicolons... EVERYWHERE, moved test from #search() into new test #retrieve()

--- server/test/product.js
+++ server/test/product.js
@@ -10,6 +10,8 @@ var productOne = {
   description : "Startlingly light large-screen tablet, with stunning HDX display, ultra-fast performance, and front and rear cameras",
   url : "http://a.abcnews.com/images/Technology/HT_Kindle_Fire_HDX_Mayday_nt_130924_16x9_992.jpg",
   categoryId: 93,
+  images : ["http://placehold.it/400x400", "http://placehold.it/200x200"],
+  shopzillaId : 300,
   price : {min: 379.99, max: 700},
   related : [16, 22, 888]
 };
@@ -19,21 +21,26 @@ var productTwo = {
   description : "Suggestive workout machine",
   url : "NSFW",
   categoryId: 22,
+  images : ["http://placehold.it/350x350"],
+  categoryId: 85,
+  shopzillaId : 3123,
   price : {min: 9.95, max: 12.47},
   related : [3]
 };
 
 /*
- * Asserts that two coupon representations are the same. The coupons can be either
- * (1) actual coupon model objects, (2) plain JavaScript objects with coupon properties,
+ * Asserts that two product representations are the same. The coupons can be either
+ * (1) actual product model objects, (2) plain JavaScript objects with product properties,
  * or (3) JSON representations returned from the API.  Because coupons from mongo can
- * have extra properties like _id and _v, we only compare the basic coupon properties.
+ * have extra properties like _id and _v, we only compare the basic product properties.
  */
 function productsShouldBeSame(product, other) {
   product.name.should.equal(other.name);
   product.description.should.equal(other.description);
   product.url.should.equal(other.url);
   product.categoryId.should.eql(other.categoryId);
+  product.shopzillaId.should.eql(other.shopzillaId);
+  product.images.join('').should.eql(other.images.join(''));
   product.price.min.should.eql(other.price.min);
   product.price.max.should.eql(other.price.max);
   product.related.join('').should.eql(other.related.join(''));
@@ -47,25 +54,25 @@ describe('Products Model', function () {
         should.not.exist(err);
         product.should.eql([]);
         done();
-      })
-    })
-  })
+      });
+    });
+  });
 
   describe('#create()', function () {
     it('should create without error', function (done) {
       Product.create(productOne, function (err) {
         should.not.exist(err);
         done();
-      })
-    })
+      });
+    });
     it('should assign all properties on creation', function (done) {
       Product.create(productOne, function (err, product) {
         should.not.exist(err);
-        productsShouldBeSame(product, productOne)
+        productsShouldBeSame(product, productOne);
         done();
-      })
-    })
-  })
+      });
+    });
+  });
 
 });
 
@@ -78,23 +85,28 @@ describe('Products Controller', function () {
         res.should.have.status(200);
         res.body.should.eql([]);
         done();
-      })
-    })
-    it('should get by id without error', function (done) {
-      // Create the product.
+      });
+    });
+  });  
+
+  describe('#retrieve()', function () {
+    it('should get by id correctly', function (done) {
+      // Create the coupon.
       request(url).post('/products').send(productOne).end(function (err, res) {
         should.not.exist(err);
         res.should.have.status(201);
+        res.should.be.json;
 
-        // Get that product by id.
+        // Get that coupon by id.
         request(url).get('/products/' + res.body._id).end(function (err, res) {
           should.not.exist(err);
           res.should.have.status(200);
+          res.should.be.json;
           done();
-        })
-      })
-    })
-  })
+        });
+      });
+    });
+  });
 
   describe('#create()', function () {
     it('should create without error', function (done) {
@@ -107,7 +119,7 @@ describe('Products Controller', function () {
     it('should assign all properties on creation, including an _id', function (done) {
       request(url).post('/products').send(productOne).end(function (err, res) {
         should.not.exist(err);
-        productsShouldBeSame(res.body, productOne)
+        productsShouldBeSame(res.body, productOne);
         done();
       })
     })
@@ -119,16 +131,23 @@ describe('Products Controller', function () {
       request(url).post('/products').send(productOne).end(function (err, res) {
         should.not.exist(err);
         res.should.have.status(201);
+        var id = res.body._id;
 
         // Delete that product.
-        request(url).del('/products/' + res.body._id).end(function (err, res) {
+        request(url).del('/products/' + id).end(function (err, res) {
           should.not.exist(err);
           res.should.have.status(200);
-          done();
-        })
-      })
-    })
-  })
+
+          // It should be deleted
+          request(url).get('/products/' + id).end(function (err, res) {
+            should.not.exist(err);
+            res.should.have.status(404);
+            done();
+          });
+        });
+      });
+    });
+  });
   
   describe('#update()', function () {
     it('should update without error', function (done) {
@@ -147,11 +166,11 @@ describe('Products Controller', function () {
           request(url).get('/products/' + id).end(function (err, res) {
             should.not.exist(err);
             res.should.have.status(200);
-            productsShouldBeSame(res.body, productTwo)
+            productsShouldBeSame(res.body, productTwo);
             done();
-          })  
-        })    
-      })
+          });  
+        });    
+      });
     });
   });
 });
