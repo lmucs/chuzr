38cd11bb480f0b5d54f3dcbca9df7e3002b77600
Quin <rqthames@yahoo.com>
2013-11-16 15:27:21 -0800

    --categories table for game

--- web-client/public/js/categoryGame.js
+++ web-client/public/js/categoryGame.js
@@ -1,3 +1,11 @@
 $(function() {
+	var categories = ["electronics", "apparel", "recreation", "tools"];
 	
+	categories.forEach(function(category){
+		$("#categoryTable").append($("<tr></tr>")
+			.append($("<td></td>")
+				.text(category)
+			)
+		)
+	});
 })
