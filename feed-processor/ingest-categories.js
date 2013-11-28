4438fed371c1777b959875736618e033acb58270
Jasmine Dahilig <jasmine.dahilig@gmail.com>
2013-11-27 16:53:43 -0800

    added taxonomy query for dynamic product ingestion

--- feed-processor/ingest-categories.js
+++ feed-processor/ingest-categories.js
@@ -1,11 +1,10 @@
 var env = process.env.NODE_ENV || 'development',
     config = require('./config/config')[env],
+
     ingest = require('./ingestor'),
     url = require('./url'),
-    category = require('./config/shopzilla/category'),
-    categoryUrl = url.generate(category, 'taxonomy');
-
+    category = require('./config/shopzilla/category')
+    categoryUrl = url.generate(category);
 
 console.log('use ' + config.db);
-
-ingest.category(categoryUrl, category.filters["format"]);
+ingest.category(categoryUrl);
--- feed-processor/ingest-products.js
+++ feed-processor/ingest-products.js
@@ -1,11 +1,42 @@
 var env = process.env.NODE_ENV || 'development',
     config = require('./config/config')[env],
+    MongoClient = require('mongodb').MongoClient;
+
     ingest = require('./ingestor'),
     url = require('./url'),
-    shopzillaProduct = require('./config/shopzilla/product'),
-    productUrl = url.generate(shopzillaProduct);
+    product = require('./config/shopzilla/product'),
+    productUrlRaw = url.generate(product);
 
 
-console.log('use ' + config.db);
+MongoClient.connect(config.dbPath, function(err, db) {
 
-ingest.shopzillaProduct(productUrl, 'json');
+  if (err) throw err;
+
+  var taxonomy = db.collection('taxonomy');
+  taxonomy.aggregate([
+    { $unwind : "$taxonomy.categories.category" },
+    { $unwind : "$taxonomy.categories.category.children.category"},
+    { $project : {
+        _id : 0,
+        "taxonomy.categories.category.children.category.id" : 1,
+        "taxonomy.categories.category.children.category.name" : 1
+      }
+    },
+    { $group : {
+      _id : 0,
+      idsAndNames : {$addToSet : "$taxonomy.categories.category.children.category"},
+      }
+    }],
+    function (err, results) {
+      var categories = results[0].idsAndNames;
+      for (c in categories) {
+        var categoryFilter = "&categoryId=" + categories[c].id;
+        var productUrl = productUrlRaw + categoryFilter;
+        console.log(productUrl);
+        ingest.shopzillaProduct(productUrl);
+      }
+      db.close();
+    }
+  );
+
+});
