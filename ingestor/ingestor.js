3afbd744b52c87b964fe5486861fea91a978cb54
Jasmine Dahilig <jasmine.dahilig@gmail.com>
2013-11-30 21:17:08 -0800

    refactored ingestor to account for differences between product-loading and taxonomy-loading

--- ingestor/ingestor.js
+++ ingestor/ingestor.js
@@ -1,32 +1,51 @@
 var http = require('http'),
     massage = require('./massager'),
-    parser = require('./parser');
+    parser = require('./parser'),
+    parse = parser["json"],
+
+    env = process.env.NODE_ENV || 'development',
+    config = require('./config/config')[env],
+    MongoClient = require('mongodb').MongoClient;
 
 var Ingestor = function () {
 
-  var _retrieveData = function (url, parse, massage) {
+  var _retrieveData = function (url, process) {
     var data = '';
     http.get(url, function (res) {
       res.on('data', function (chunk) {
         data += chunk;
       });
       res.on('end', function () {
-        var result = parse(data);
-        console.log(massage(result));
+        process(data);
       });
     }).on('error', function (err) {
     });
   };
 
-  var _generateIngestionProcess = function (massageProcess) {
-    return function (url) {
-      _retrieveData(url, parser["json"], massageProcess);
-    };
+  var _products = function (url) {
+    _retrieveData(url, function (data) {
+      var result = parse(data),
+          chuzrProducts = massage.products(result);
+      console.log('use ' + config.db);
+      for (p in chuzrProducts) {
+        var product = JSON.stringify(chuzrProducts[p]);
+        console.log('db.products.insert(' + product + ');');
+      }
+    });
+  };
+
+  var _taxonomy = function (url) {
+    _retrieveData(url, function (data) {
+      var result = parse(data),
+          shopzillaTaxonomy = massage.taxonomy(result);
+      console.log('use ' + config.db);
+      console.log('db.taxonomy.insert(%j);', shopzillaTaxonomy);
+    });
   };
 
   return {
-    products: _generateIngestionProcess(massage.products),
-    taxonomy: _generateIngestionProcess(massage.taxonomy)
+    products: _products,
+    taxonomy: _taxonomy
   };
 }();
 
