995ef49495b00fcd99f494933e001fea74069c7b
Jasmine Dahilig <jasmine.dahilig@gmail.com>
2013-11-30 21:15:39 -0800

    moved db loading tasks into ingestor

--- ingestor/ingest-categories.js
+++ ingestor/ingest-categories.js
@@ -1,10 +1,6 @@
-var env = process.env.NODE_ENV || 'development',
-    config = require('./config/config')[env],
-
-    ingest = require('./ingestor'),
+var ingest = require('./ingestor'),
     url = require('./url'),
     category = require('./config/shopzilla/category')
     categoryUrl = url.generate(category);
 
-console.log('use ' + config.db);
-ingest.category(categoryUrl);
+ingest.taxonomy(categoryUrl);
