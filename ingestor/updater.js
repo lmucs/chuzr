d1223cabc5d460ff62d6cc672493b6bc313bd491
Danny Gibson <dgibson8@lion.lmu.edu>
2013-12-04 02:41:04 -0800

    name comparing of ftp files vs current db state

--- ingestor/updater.js
+++ ingestor/updater.js
@@ -14,8 +14,7 @@ if (dd < 10) dd = '0' + dd;
 var mm = today.getMonth() + 1;
 if (mm < 10) mm = '0' + mm;
 var yyyy = today.getFullYear();
-var currentFtpValue = yyyy + mm + dd + '00';
-
+var currentFtpValue = yyyy.toString() + mm.toString() + dd.toString() + '00';
 var c = new ftpClient();
 
 MongoClient.connect(config.dbPath, function(err, db) {
@@ -24,11 +23,11 @@ MongoClient.connect(config.dbPath, function(err, db) {
   deltas.drop();
     c.on('ready', function () {
     c.list('./xml-delta-1.0/', function(err, list) {
-        console.log(list); 
-        for ( l in list ) {
-          if (+l.name > +currentFtpValue){
+        for (var i = 0; i < list.length; i++ ) {
+          if (+list[i].name > +currentFtpValue){
+          console.log(+list[i].name);
           currentFtpValue++;
-          c.get(l.name + '/publisher_all_updates.xml', function(err, stream) {
+          c.get(list[i].name + '/publisher_all_updates.xml', function(err, stream) {
             if(err) throw err;
             var updates = stream.read();
             deltas.insert(updates, function(err, result) {
