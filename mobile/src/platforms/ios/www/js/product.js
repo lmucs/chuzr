c6c30adc8a0fb98b7b834c5efe2cd9b95bbb8bd3
TMoore <tmoore19@lion.lmu.edu>
2013-12-05 07:47:30 -0800

    Added necessary files and converted jade to html for layout.

---
+++ mobile/src/platforms/ios/www/js/product.js
@@ -0,0 +1,28 @@
+//product.js
+
+$(function() {
+	var httpGet = function(theUrl) {
+		    var xmlHttp = null;
+
+		    xmlHttp = new XMLHttpRequest();
+		    xmlHttp.open( "GET", theUrl, false );
+		    xmlHttp.send( null );
+		    return xmlHttp.responseText;
+		},
+
+		productId = $(".product-name").attr('product-id'),
+		product = jQuery.parseJSON(httpGet(Config.getApiBaseUrl() + "/products/" + productId)),
+
+		populateData = function (product) {
+		    console.log(Config.getApiBaseUrl() + "/products/" + productId);
+		    console.log(product);
+		    $(".product-name").text(product.name);
+		    $(".product-image-rating img").attr('src', product.images[0]);
+		    $(".url-to-purchase a").attr('href', product.url);
+		    $(".product-description .description").text(product.description);
+		    $(".current-product-rating .rating").text(product.rating);
+		    $(".current-product-rating a").attr('href', '/rateme/' + productId);
+        };
+	
+	populateData(product);
+})
