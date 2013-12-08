48c35fa7243b695dc0f77801adb7ed6488d467bb
Britainjs <bsouthwi@lion.lmu.edu>
2013-12-08 04:15:06 -0800

    Modified the login to post to sessions. Seems to work, but one of the redirect paths doesnt exist yet.

--- web-client/public/js/login.js
+++ web-client/public/js/login.js
@@ -1,21 +1,15 @@
 $(function(){
 
   $("#signin").click(function(){
-    //userEmailLookup looks up and returns userID based on email address -- not implemented
-    var userID = userEmailLookup($("#inputEmail").val());
+    var email = $("#inputEmail").val();
+    var pass = $("#inputPassword").val();
+    $.post("http://localhost:3000/sessions", {email: email, pass: pass})
+      .done(function(){
+        window.location.replace('/home');
 
-    $.ajax({
-      url: Config.getApiBaseUrl() + "/users" + userID,
-      cache: none
-    })
-      .done(function(user){
-        if($("#inputPassword").val() === user.hashedPassword) {
-          currentUser.userID = userID;
-          window.location.replace("/home");
-        } else {
-          window.location.replace("/");
-        }
-
+      })
+      .fail(function() {
+        window.location.replace('/');
       })
   });
 });
