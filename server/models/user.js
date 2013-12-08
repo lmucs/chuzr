52c675595153f178b94b70256a41319ef6c6dbc0
Ray Toal <rtoal@lmu.edu>
2013-12-08 09:17:26 -0800

    Holwaychuk is cool but we don't need to follow his style

--- server/models/user.js
+++ server/models/user.js
@@ -1,43 +1,41 @@
-var mongoose = require('mongoose')
-  , Schema = mongoose.Schema
-  , ObjectId = Schema.ObjectId
-  , bcrypt = require('bcrypt-nodejs')
-  , workFactor = 10;
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+var workFactor = 10;
 
-var userSchema = new Schema({
-    name            : {
-        first   : String
-      , last    : { 
-            type    : String
-          , trim: true 
-        }
+var userSchema = new mongoose.Schema({
+  name: {
+    first: String,
+    last: { 
+      type: String,
+      trim: true 
     }
-  , email           : String
-  , login           : {
-          type: String
-        , unique: true
-    }
-  , hashedPassword  : String
-  , avatarURL       : String
-  , reputation      : Number
-  , socialHandle    : String
-  , dateOfBirth     : Date
-  , location        : {
-            city    : String
-          , state   : String
-    }
-  , friends         : [ObjectId]
-  , isAdmin         : Boolean
+  },
+  email: String,
+  login: {
+    type: String,
+    unique: true
+  },
+  hashedPassword: String,
+  avatarURL: String,
+  reputation: Number,
+  socialHandle: String,
+  dateOfBirth: Date,
+  location: {
+    city: String,
+    state: String
+  },
+  friends: [String],
+  isAdmin: Boolean,
 });
 
-// A pre hook to hash the password on save.
+// A pre hook to hash the password on save
 userSchema.pre('save', function (next) {
   var user = this;
   
   //don't bother hashing if no changes were made
   if (!user.isModified('hashedPassword')) return next();
   
-  bcrypt.hash(user.hashedPassword, null, null, function(err, hash) {
+  bcrypt.hash(user.hashedPassword, null, null, function (err, hash) {
     if (err) return next(err);
     user.hashedPassword = hash;
     next();
